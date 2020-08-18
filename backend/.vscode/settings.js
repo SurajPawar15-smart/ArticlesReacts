4862ab987a17349bc492618f6b4feb112d4c9ec3
SurajPawar15-smart <supersuraj143@gmail.com>
2020-08-18 14:29:37 +0530

    first commit

---
+++ backend/.vscode/settings.js
@@ -0,0 +1,24 @@
+// {   
+//     "python.pythonPath":
+//     "C:\\Users\\pw\\AppData\\Local\\Programs\\Python\\Python37\\Scripts",
+//     "editor.formatOnSave": true,
+//     "python.linting.pep8Enabled": true,
+//     "python.linting.pylintPath": "pylint",
+//     "python.linting.pylintArgs": ["--load-plugins","pylint_django"],
+//     "python.linting.pylintEnabled": true,
+//     "python.venvPath":"${C:\\Users\\pw\\Desktop\\pdata}/backend/env/bin/python3",
+//     "python.linting.pep8Args":["--ignore=E501"],
+    
+// }
+{
+    "python.pythonPath":"C:\\Users\\pw\\AppData\\Local\\Programs\\Python\\Python37\\Scripts",
+    "editor.formatOnSave": true,
+    "python.linting.pep8Enabled": true,
+    "python.linting.pylintPath": "pylint",
+    "python.linting.pylintArgs": ["--load-plugins", "pylint_django"],
+    "python.linting.pylintEnabled": true,
+    "python.venvPath": "${C:\\Users\\pw\\Desktop\\ppdate}/env/bin/python3",
+    "python.linting.pep8Args": ["--ignore=E501"],
+    
+    
+}
---
+++ backend/pdata/articles/api/views.py
@@ -0,0 +1,62 @@
+# from rest_framework.generics import (
+#     ListAPIView,
+#     CreateAPIView,
+#     RetrieveAPIView
+    
+# )
+# from articles.models import Article
+# from .serializers import ArticleSerializer
+
+# class ArticleListView(ListAPIView):
+#     queryset=Article.objects.all()
+#     serializer_class=ArticleSerializer
+
+# class ArticleCreateView(CreateAPIView):
+#     queryset=Article.objects.all()
+#     serializer_class=ArticleSerializer
+
+# class ArticleDetailView(RetrieveAPIView):
+#     queryset=Article.objects.all()
+#     serializer_class=ArticleSerializer
+
+#  or
+
+
+# from rest_framework.generics import (
+#     ListAPIView,
+#     RetrieveAPIView,
+#     CreateAPIView,
+#     UpdateAPIView,
+#     DestroyAPIView
+# )
+# from articles.models import Article
+# from .serializers import ArticleSerializer
+
+# class ArticleListView(ListAPIView):
+#     queryset=Article.objects.all()
+#     serializer_class=ArticleSerializer
+
+# class ArticleDetailView(RetrieveAPIView):
+#     queryset=Article.objects.all()
+#     serializer_class=ArticleSerializer
+
+# class ArticleCreateView(CreateAPIView):
+#     queryset=Article.objects.all()
+#     serializer_class=ArticleSerializer
+
+# class ArticleUpdateView(UpdateAPIView):
+#     queryset=Article.objects.all()
+#     serializer_class=ArticleSerializer
+
+# class ArticleDeleteView(DestroyAPIView):
+#     queryset=Article.objects.all()
+#     serializer_class=ArticleSerializer
+    
+from rest_framework import viewsets    
+from articles.models import Article
+from .serializers import ArticleSerializer
+
+
+class ArticleViewSet(viewsets.ModelViewSet):
+    serializer_class = ArticleSerializer
+    queryset = Article.objects.all()
